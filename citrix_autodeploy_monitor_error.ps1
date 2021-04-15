b2df5d644dfabcb27cda1a5f57c2bb74cd076601
Tony Sathre SU <SU89944@oshkoshglobal.com>
2021-04-15 10:37:29 -0500

    changed to use @splat

--- citrix_autodeploy_monitor_error.ps1
+++ citrix_autodeploy_monitor_error.ps1
@@ -1,8 +1,12 @@
 $Json = Get-Content $PSScriptRoot\citrix_autodeploy_config_email.json | ConvertFrom-Json
 $Event = Get-EventLog -LogName 'Citrix Autodeploy' -Newest 1 -InstanceId 1
-$Body = $Event.Message
-$From = $Json.From
-$To = $Json.To
-$Subject = 'Citrix Autodeploy Error Occurred'
-$SmtpServer = $Json.SmtpServer
-Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer $SmtpServer
+
+$MailParams = @{
+    $SmtpServer = $Json.SmtpServer
+    $To         = $Json.To
+    $From       = $Json.From
+    $Subject    = 'Citrix Autodeploy Error Occurred'
+    $Body       = $Event.Message
+}
+
+Send-MailMessage @MailParams
--- citrix_autodeploy_monitor_machine_creation.ps1
+++ citrix_autodeploy_monitor_machine_creation.ps1
@@ -1,8 +1,12 @@
 $Json = Get-Content $PSScriptRoot\citrix_autodeploy_config_email.json | ConvertFrom-Json
 $Event = Get-EventLog -LogName 'Citrix Autodeploy' -Newest 1 -InstanceId 3
-$Body = $Event.Message
-$From = $Json.From
-$To = $Json.To
-$Subject = 'Citrix Autodeploy Machine Created'
-$SmtpServer = $Json.SmtpServer
-Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer $SmtpServer
+
+$MailParams = @{
+    $SmtpServer = $Json.SmtpServer
+    $To         = $Json.To
+    $From       = $Json.From
+    $Subject    = 'Citrix Autodeploy Machine Created'
+    $Body       = $Event.Message
+}
+
+Send-MailMessage @MailParams
