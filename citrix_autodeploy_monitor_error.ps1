579c233ea440c04d1e0dc035e7010bcf88029c13
tonysathre <tony@ghostbit.org>
2021-05-12 16:05:18 -0500

    Fixed typo

--- citrix_autodeploy_monitor_error.ps1
+++ citrix_autodeploy_monitor_error.ps1
@@ -2,11 +2,11 @@ $Json = Get-Content $PSScriptRoot\citrix_autodeploy_config_email.json | ConvertF
 $Event = Get-EventLog -LogName 'Citrix Autodeploy' -Newest 1 -InstanceId 1
 
 $MailParams = @{
-    $SmtpServer = $Json.SmtpServer
-    $To         = $Json.To
-    $From       = $Json.From
-    $Subject    = 'Citrix Autodeploy Error Occurred'
-    $Body       = $Event.Message
+    SmtpServer = $Json.SmtpServer
+    To         = $Json.To
+    From       = $Json.From
+    Subject    = 'Citrix Autodeploy Error Occurred'
+    Body       = $Event.Message
 }
 
 Send-MailMessage @MailParams
