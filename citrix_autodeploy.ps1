2c0548bf54ba6917e0ec00cdd3d89e5f2b5f3a99
tonysathre <tony@ghostbit.org>
2024-09-13 13:25:53 -0500

    cleanup

--- citrix_autodeploy.ps1
+++ citrix_autodeploy.ps1
@@ -15,22 +15,17 @@ $Config = Get-Config -FilePath $ConfigFilePath
 
 foreach ($AutodeployMonitor in $Config.AutodeployMonitors.AutodeployMonitor) {
     $MonitorDetails = $AutodeployMonitor | Format-List | Out-String
-    Write-CitrixAutoDeployLog -Message "Autodeploy job started: $MonitorDetails" -EventId 0 -EntryType Information
 
-    try {
-        $AdminAddress       = $AutodeployMonitor.AdminAddress
-        $BrokerCatalog      = Get-BrokerCatalog -AdminAddress $AdminAddress -Name $AutodeployMonitor.BrokerCatalog
-        $DesktopGroupName   = Get-BrokerDesktopGroup -AdminAddress $AdminAddress -Name $AutodeployMonitor.DesktopGroupName
-        $UnassignedMachines = Get-BrokerDesktop -AdminAddress $AdminAddress -DesktopGroupName $DesktopGroupName.Name -IsAssigned $false
-        $MachinesToAdd      = $AutodeployMonitor.MinAvailableMachines - $UnassignedMachines.Count
-        $PreTask            = $AutodeployMonitor.PreTask
-        $PostTask           = $AutodeployMonitor.PostTask
-    }
-    catch {
-        Write-CitrixAutoDeployLog -Message "$($_.Exception.Message)`n`n$($_.Exception.StackTrace)" -EventId 1 -EntryType Error
-        break
-    }
-    write-host "Machines to add: $MachinesToAdd"
+    Write-CitrixAutoDeployLog -Message "Autodeploy job started: ${MonitorDetails}" -EventId 0 -EntryType Information
+
+    $AdminAddress       = $AutodeployMonitor.AdminAddress
+    $BrokerCatalog      = Get-BrokerCatalog -AdminAddress $AdminAddress -Name $AutodeployMonitor.BrokerCatalog
+    $DesktopGroup       = Get-BrokerDesktopGroup -AdminAddress $AdminAddress -Name $AutodeployMonitor.DesktopGroupName
+    $UnassignedMachines = Get-BrokerMachine -AdminAddress $AdminAddress -DesktopGroupName $DesktopGroup.Name -IsAssigned $false
+    $MachinesToAdd      = $AutodeployMonitor.MinAvailableMachines - $UnassignedMachines.Count
+    $PreTask            = $AutodeployMonitor.PreTask
+    $PostTask           = $AutodeployMonitor.PostTask
+
     while ($MachinesToAdd -gt 0) {
         try {
             Add-Machine -AdminAddress $AdminAddress -BrokerCatalog $BrokerCatalog -DesktopGroup $DesktopGroup -PreTask $PreTask -PostTask $PostTask
