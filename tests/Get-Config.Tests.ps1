34af87ad20d5dc191fdce01b42583a6a786d73a1
Tony Sathre <tony@ghostbit.org>
2024-09-12 21:44:48 -0500

    Import our module CitrixAutodeploy

--- tests/Get-Config.Tests.ps1
+++ tests/Get-Config.Tests.ps1
@@ -1,4 +1,8 @@
 Describe 'Get-Config' {
+    BeforeAll {
+        Import-Module ${PSScriptRoot}\..\module\CitrixAutodeploy -Force -ErrorAction Stop -DisableNameChecking -WarningAction SilentlyContinue
+    }
+
     It 'Should return configuration object if file exists' {
         $FilePath = "${PSScriptRoot}/test_config.json"
         $Config = Get-Config -FilePath $FilePath
@@ -9,6 +13,6 @@ Describe 'Get-Config' {
 
     It 'Should throw error if file does not exist' {
         $FilePath = "${PSScriptRoot}/non_existent_config.json"
-        { Get-Config -FilePath $FilePath -ErrorAction SilentlyContinue -ErrorVariable Errors } | Should -Throw
+        { Get-Config -FilePath $FilePath -ErrorAction SilentlyContinue -ErrorVariable Errors } | Should -Throw # suppress error message so it doesn't pollute our terminal
     }
 }
