bc8e2f890c51aad3a9bdb3a9d2a962a4276745ad
tonysathre <tony@ghostbit.org>
2024-10-01 13:17:24 -0500

    update build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,22 @@
-Import-Module PowerShellGet -ErrorAction Stop
+Import-Module Microsoft.PowerShell.PSResourceGet -ErrorAction Stop
 
-$BasePath = "${PSScriptRoot}\module\CitrixAutodeploy"
-$NestedModules = (Get-ChildItem ${BasePath}\functions\*.ps1).Name | ForEach-Object { ".\functions\$_" }
+$BasePath          = "${PSScriptRoot}\module\CitrixAutodeploy"
+$NestedModules     = (Get-ChildItem ${BasePath}\functions\*.ps1).Name | ForEach-Object { ".\functions\$_" }
 $FunctionsToExport = (Get-ChildItem ${BasePath}\functions\*.ps1).Name -replace '\.ps1$'
+$Author            = 'Tony Sathre'
+$CompanyName       = 'Tony Sathre'
+$Description       = 'This module is used to automate the deployment of Citrix virtual desktops.'
 
-Update-ModuleManifest -Path ${BasePath}\CitrixAutodeploy.psd1 -NestedModules $NestedModules -FunctionsToExport $FunctionsToExport
+
+$ModuleManifest = @{
+    Author            = $Author
+    CompanyName       = $CompanyName
+    Description       = $Description
+    Path              = "${BasePath}\CitrixAutodeploy.psd1"
+    FunctionsToExport = $FunctionsToExport
+    NestedModules     = $NestedModules
+}
+
+Update-ModuleManifest @ModuleManifest
+
+
