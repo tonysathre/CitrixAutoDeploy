73ca83c189dd6a70bb640b191cc907c5500131f5
tonysathre <tony@ghostbit.org>
2024-10-01 13:50:53 -0500

    fix tests

--- tests/Invoke-CtxAutodeployTask.Tests.ps1
+++ tests/Invoke-CtxAutodeployTask.Tests.ps1
@@ -3,21 +3,41 @@ Describe 'Invoke-CtxAutodeployTask' {
         Import-Module ${PSScriptRoot}\..\module\CitrixAutodeploy -Force -ErrorAction Stop -DisableNameChecking -WarningAction SilentlyContinue
     }
 
+    $TestConfig = Get-CtxAutodeployConfig -FilePath ${PSScriptRoot}\test_config.json
     $TestCases = @(
-        @{ Name = 'PreTask';  Type = 'Pre'  },
-        @{ Name = 'PostTask'; Type = 'Post' }
+        @{
+            Task = 'PreTask'
+            Type = 'Pre'
+            ArgumentList = @(
+                [PSCustomObject]@{
+                    AutodeployMonitor = $TestConfig.AutodeployMonitors.AutodeployMonitor[0]
+                    DesktopGroup      =  $TestConfig.AutodeployMonitors.AutodeployMonitor[0].DesktopGroupName
+                    BrokerCatalog     = $TestConfig.AutodeployMonitors.AutodeployMonitor[0].BrokerCatalog
+                }
+            )
+        },
+        @{
+            Task = 'PostTask'
+            Type = 'Post'
+            ArgumentList = [PSCustomObject]@{
+                AutodeployMonitor = $TestConfig.AutodeployMonitors.AutodeployMonitor[0]
+                DesktopGroup      = $TestConfig.AutodeployMonitors.AutodeployMonitor[0].DesktopGroupName
+                BrokerCatalog     = $TestConfig.AutodeployMonitors.AutodeployMonitor[0].BrokerCatalog
+            }
+        }
     )
 
-    It 'Should execute <_.Name>' -TestCases $TestCases {
-        param($Name, $Type)
+    It 'Should execute <_.Task>' -TestCases $TestCases {
+        param($Task, $Type, $ArgumentList)
 
-        $ExpectedOutput = "A test ${Name} script was executed"
+        $ExpectedOutput = "A test ${Task} script was executed"
         $Task           = "${PSScriptRoot}\test_${Name}.ps1"
 
         $Params = @{
             Task    = $Task
-            Context = 'test-machine-name-for-context'
+            Context = 'TestContext'
             Type    = $Type
+            ArgumentList = $ArgumentList
         }
 
         Set-Content -Path $Task -Value "'${ExpectedOutput}'"
@@ -25,42 +45,4 @@ Describe 'Invoke-CtxAutodeployTask' {
         $ActualOutput = Invoke-CtxAutodeployTask @Params
         $ActualOutput | Should -Be $ExpectedOutput
     }
-
-    # It 'Should execute pre-task script' {
-    #     $ExpectedOutput = 'A test pre-task script was executed'
-    #     $PreTask        = "${PSScriptRoot}\test_pre_task.ps1"
-    #     $MachineName    = 'test-machine-name'
-
-    #     Set-Content -Path $PreTask -Value "'${ExpectedOutput}'"
-
-    #     $ActualOutput = Invoke-CtxAutodeployTask -PreTask $PreTask -MachineName $MachineName -Type Pre
-    #     $ActualOutput | Should -Be $ExpectedOutput
-    # }
-
-    # It 'Should execute post-task script' {
-    #     $ExpectedOutput = 'A test post-task script was executed'
-    #     $PostTask       = "${PSScriptRoot}\test_post_task.ps1"
-    #     $MachineName    = 'test-machine-name'
-
-    #     Set-Content -Path $PreTask -Value "'${ExpectedOutput}'"
-
-    #     $ActualOutput = Invoke-CtxAutodeployTask -Task $PostTask -MachineName $MachineName -Type Post
-    #     $ActualOutput | Should -Be $ExpectedOutput
-    # }
-
-    # It 'Should throw error if pre-task script does not exist' {
-    #     $PreTask     = "${PSScriptRoot}\non_existent_pre_task.ps1"
-    #     $MachineName = 'test-machine-name'
-
-    #     { Invoke-PreTask -PreTask $PreTask -MachineName $MachineName -ErrorVariable Errors } | Should -Throw
-    # }
-
-    # It 'Should throw error if pre-task script fails' {
-    #     $PreTask     = "${PSScriptRoot}\test_pre_task.ps1"
-    #     $MachineName = 'test-machine-name'
-
-    #     Set-Content -Path $PreTask -Value 'throw "Test error"'
-
-    #     { Invoke-PreTask -PreTask $PreTask -MachineName $MachineName } | Should -Throw
-    # }
 }
