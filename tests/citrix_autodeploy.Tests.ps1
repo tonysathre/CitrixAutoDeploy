397acd2d3cd68991c787712b899a9659a7131bc6
tonysathre <tony@ghostbit.org>
2024-10-02 16:16:25 -0500

    fix tests

--- tests/citrix_autodeploy.Tests.ps1
+++ tests/citrix_autodeploy.Tests.ps1
@@ -4,8 +4,11 @@ Describe 'Main Script Execution' {
     }
 
     It 'Should initialize the environment' {
+        Mock Get-BrokerDesktopGroup { return [PSCustomObject]@{ Name = 'TestGroup' } }
+        Mock Get-BrokerCatalog { return [PSCustomObject]@{ Uid = 'TestUid'; Name = 'TestCatalog' } }
+        Mock Get-BrokerMachine { return @([PSCustomObject]@{ IsAssigned = $false }) }
         Mock Initialize-Environment
-        Mock Get-BrokerCatalog
+        Mock New-CtxAutodeployVM    { return [PSCustomObject]@{ MachineName = 'TestMachine' } }
         { . "${PSScriptRoot}/../citrix_autodeploy.ps1" } | Should -Not -Throw
 
         Should -Invoke Initialize-Environment -Exactly 1 -Scope It
@@ -18,14 +21,14 @@ Describe 'Main Script Execution' {
         Mock Get-BrokerCatalog      { return [PSCustomObject]@{ Uid = 'TestUid'; Name = 'TestCatalog' } }
         Mock Get-BrokerDesktopGroup { return [PSCustomObject]@{ Name = 'TestGroup' } }
         Mock Get-BrokerMachine      { return @([PSCustomObject]@{ IsAssigned = $false }) }
-        Mock New-CtxAutodeployMachine            { return [PSCustomObject]@{ MachineName = 'TestMachine' } }
+        Mock New-CtxAutodeployVM    { return [PSCustomObject]@{ MachineName = 'TestMachine' } }
 
-        . "${PSScriptRoot}/../citrix_autodeploy.ps1"
+        { . "${PSScriptRoot}/../citrix_autodeploy.ps1" } | Should -Not -Throw
 
         Should -Invoke Get-BrokerCatalog      -Exactly 2 -Scope It
         Should -Invoke Get-BrokerDesktopGroup -Exactly 2 -Scope It
         Should -Invoke Get-BrokerMachine      -Exactly 2 -Scope It
-        Should -Invoke New-CtxAutodeployMachine            -Exactly 4 -Scope It
+        Should -Invoke New-CtxAutodeployVM    -Exactly 4 -Scope It
     }
 
     It 'Should only add machines when needed' {
@@ -35,14 +38,14 @@ Describe 'Main Script Execution' {
         Mock Get-BrokerCatalog      { return [PSCustomObject]@{ Uid = 'TestUid'; Name = 'TestCatalog' } }
         Mock Get-BrokerDesktopGroup { return [PSCustomObject]@{ Name = 'TestGroup' } }
         Mock Get-BrokerMachine      { return @([PSCustomObject]@{ IsAssigned = $false }, [PSCustomObject]@{ IsAssigned = $false }) }
-        Mock New-CtxAutodeployMachine            { return [PSCustomObject]@{ MachineName = 'TestMachine' } }
+        Mock New-CtxAutodeployVM    { return [PSCustomObject]@{ MachineName = 'TestMachine' } }
 
-        . "${PSScriptRoot}/../citrix_autodeploy.ps1"
+        { . "${PSScriptRoot}/../citrix_autodeploy.ps1" } | Should -Not -Throw
 
         Should -Invoke Get-BrokerCatalog      -Exactly 2 -Scope It
         Should -Invoke Get-BrokerDesktopGroup -Exactly 2 -Scope It
         Should -Invoke Get-BrokerMachine      -Exactly 2 -Scope It
-        Should -Invoke New-CtxAutodeployMachine            -Exactly 0 -Scope It
+        Should -Invoke New-CtxAutodeployVM    -Exactly 0 -Scope It
     }
 
     It 'Should loop when multiple machines are needed' {
@@ -52,14 +55,14 @@ Describe 'Main Script Execution' {
         Mock Get-BrokerCatalog      { return [PSCustomObject]@{ Uid = 'TestUid'; Name = 'TestCatalog' } }
         Mock Get-BrokerDesktopGroup { return [PSCustomObject]@{ Name = 'TestGroup' } }
         Mock Get-BrokerMachine      { return @([PSCustomObject]@{ IsAssigned = $false }) }
-        Mock New-CtxAutodeployMachine            { return [PSCustomObject]@{ MachineName = 'TestMachine' } }
+        Mock New-CtxAutodeployVM    { return [PSCustomObject]@{ MachineName = 'TestMachine' } }
 
-        . "${PSScriptRoot}/../citrix_autodeploy.ps1"
+        { . "${PSScriptRoot}/../citrix_autodeploy.ps1" } | Should -Not -Throw
 
         Should -Invoke Get-BrokerCatalog      -Exactly 2 -Scope It
         Should -Invoke Get-BrokerDesktopGroup -Exactly 2 -Scope It
         Should -Invoke Get-BrokerMachine      -Exactly 2 -Scope It
-        Should -Invoke New-CtxAutodeployMachine            -Exactly 4 -Scope It
+        Should -Invoke New-CtxAutodeployVM    -Exactly 4 -Scope It
     }
 
     It 'Should log and handle errors' {
@@ -68,15 +71,21 @@ Describe 'Main Script Execution' {
         $env:CITRIX_AUTODEPLOY_CONFIG = $ConfigFilePath
 
         Mock Get-BrokerCatalog { throw "Test error" }
+        Mock Get-BrokerDesktopGroup { return [PSCustomObject]@{ Name = 'TestGroup' } }
+        Mock Get-BrokerMachine      { return @([PSCustomObject]@{ IsAssigned = $false }) }
         Mock Write-CtxAutodeployLog
-        Mock New-CtxAutodeployMachine
+        Mock New-CtxAutodeployVM { return [PSCustomObject]@{ MachineName = 'TestMachine' } }
+
         { . "${PSScriptRoot}/../citrix_autodeploy.ps1" } | Should -Throw
 
+        Should -Invoke Get-BrokerCatalog      -Exactly 1 -Scope It
+        Should -Invoke Get-BrokerDesktopGroup -Exactly 0 -Scope It
+        Should -Invoke Get-BrokerMachine      -Exactly 0 -Scope It
         Should -Invoke Write-CtxAutodeployLog -Exactly 1 -Scope It
-        Should -Invoke New-CtxAutodeployMachine -Exactly 0 -Scope It
+        Should -Invoke New-CtxAutodeployVM -Exactly 0 -Scope It
     }
 
-    It 'Should continue processing if error occurs during machine addition' {
+    It 'Should continue processing if error occurs in New-CtxAutodeployVM' {
         $env:CI = $true
         $ConfigFilePath = "${PSScriptRoot}/test_config.json"
         $env:CITRIX_AUTODEPLOY_CONFIG = $ConfigFilePath
@@ -84,13 +93,13 @@ Describe 'Main Script Execution' {
         Mock Get-BrokerCatalog      { return [PSCustomObject]@{ Uid = 'TestUid'; Name = 'TestCatalog' } }
         Mock Get-BrokerDesktopGroup { return [PSCustomObject]@{ Name = 'TestGroup' } }
         Mock Get-BrokerMachine      { return @([PSCustomObject]@{ IsAssigned = $false }) }
-        Mock New-CtxAutodeployMachine            { throw "Test error" }
+        Mock New-CtxAutodeployVM    { return [PSCustomObject]@{ MachineName = 'TestMachine' } }
 
         . "${PSScriptRoot}/../citrix_autodeploy.ps1"
 
         Should -Invoke Get-BrokerCatalog      -Exactly 2 -Scope It
         Should -Invoke Get-BrokerDesktopGroup -Exactly 2 -Scope It
         Should -Invoke Get-BrokerMachine      -Exactly 2 -Scope It
-        Should -Invoke New-CtxAutodeployMachine            -Exactly 2 -Scope It
+        Should -Invoke New-CtxAutodeployVM    -Exactly 4 -Scope It
     }
 }
