891843c7b85708280645741d5dc460fda5c63d66
tonysathre <tony@ghostbit.org>
2024-10-02 16:15:15 -0500

    prepend timestamp to log output

--- module/CitrixAutodeploy/functions/Write-CtxAutodeployLog.ps1
+++ module/CitrixAutodeploy/functions/Write-CtxAutodeployLog.ps1
@@ -1,18 +1,25 @@
 function Write-CtxAutodeployLog {
     [CmdletBinding()]
     param(
+        [Parameter(Mandatory)]
         [psobject]$Message,
+
+        [Parameter(Mandatory)]
         [int]$EventId,
+
+        [Parameter(Mandatory)]
         [System.Diagnostics.EventLogEntryType]$EntryType
     )
 
     if ($env:CI) {
         if ($EntryType -eq [System.Diagnostics.EventLogEntryType]::Error) {
-            Write-Error $Message
+            Write-Error ('{0} {1}' -f "[$([datetime]::Now)]", $Message)
         } else {
-            Write-Host $Message
+            Write-Host ('{0} {1}' -f "[$([datetime]::Now)]", $Message)
         }
-    } else {
-        Write-EventLog -LogName 'Citrix Autodeploy' -Source 'Citrix Autodeploy' -Message $Message -EventId $EventId -EntryType $EntryType
+
+        return
     }
+
+    Write-EventLog -LogName 'Citrix Autodeploy' -Source 'Citrix Autodeploy' -Message $Message -EventId $EventId -EntryType $EntryType
 }
