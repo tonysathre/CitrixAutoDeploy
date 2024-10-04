75c39e77cf386145a7907ce271356cf922e91186
tonysathre <tony@ghostbit.org>
2024-10-04 11:03:37 -0500

    wip

--- tests/Write-CitrixAutoDeployLog.Tests.ps1
+++ tests/Write-CitrixAutoDeployLog.Tests.ps1
@@ -1,37 +1,51 @@
+# FILE: module/CitrixAutodeploy/functions/Write-CtxAutodeployLog.Tests.ps1
+
 Describe 'Write-CtxAutodeployLog' {
     BeforeAll {
-        Import-Module ${PSScriptRoot}\..\module\CitrixAutodeploy -Force -ErrorAction Stop -DisableNameChecking -WarningAction SilentlyContinue
+        Import-Module "${PSScriptRoot}/../module/CitrixAutodeploy" -Force -ErrorAction Stop -DisableNameChecking -WarningAction SilentlyContinue
     }
 
-    It 'Should write error message to error stream in CI environment' {
-        $env:CI = $true
-        $Message = "Test error message written to error stream"
-        $EventId = 1
-        $EntryType = [System.Diagnostics.EventLogEntryType]::Error
+    Context 'CI environment' {
+        It 'Should write message to host' {
+            $env:CI = $true
+            $Params = @{
+                Message   = 'Test message written to stdout'
+                EventId   = 2
+                EntryType = [System.Diagnostics.EventLogEntryType]::Information
+            }
 
-        { Write-CtxAutodeployLog -Message $Message -EventId $EventId -EntryType $EntryType -ErrorVariable Errors -ErrorAction SilentlyContinue
-            $Errors.Count | Should Be 1
-            $Errors[0].Exception.Message | Should Be (
-                $Message
-            )
+            { Write-CtxAutodeployLog @Params } | Should -Not -Throw
         }
     }
 
-    It 'Should write message to host in CI environment' {
-        $env:CI = $true
-        $Message = "Test message written to stdout"
-        $EventId = 2
-        $EntryType = [System.Diagnostics.EventLogEntryType]::Information
+    Context 'Non-CI environment' {
+        It 'Should write to Windows event log in non-CI environment' {
+            $env:CI = $false
+            $Params = @{
+                Message   = 'Test event log message'
+                EventId   = 3
+                EntryType = [System.Diagnostics.EventLogEntryType]::Information
+            }
 
-        { Write-CtxAutodeployLog -Message $Message -EventId $EventId -EntryType $EntryType } | Should -Not -Throw
-    }
+            Mock Write-EventLog
 
-    It 'Should write to Windows event log in non-CI environment' {
-        $env:CI = $false
-        $Message = "Test event log message"
-        $EventId = 3
-        $EntryType = [System.Diagnostics.EventLogEntryType]::Information
+            { Write-CtxAutodeployLog @Params } | Should -Not -Throw
 
-        { Write-CtxAutodeployLog -Message $Message -EventId $EventId -EntryType $EntryType } | Should -Not -Throw
+            Should -Invoke Write-EventLog -Exactly 1 -Scope It
+        }
+        It 'Should not write to Windows event log if EventId is missing in non-CI environment' {
+            $env:CI = $false
+            $Params = @{
+                Message   = 'Test message without EventId'
+                EventId   = 6
+                EntryType = [System.Diagnostics.EventLogEntryType]::Information
+            }
+
+            Mock Write-EventLog
+
+            { Write-CtxAutodeployLog @Params } | Should -Not -Throw
+
+            Should -Not -Invoke Write-EventLog -Scope It
+        }
     }
 }
