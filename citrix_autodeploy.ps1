c57b2fa03a3f8a7be7cd353275901addd05e858c
tonysathre <tony@ghostbit.org>
2024-10-08 12:27:48 -0500

    Restructure

--- citrix_autodeploy.ps1
+++ citrix_autodeploy.ps1
@@ -3,7 +3,7 @@
 param (
     [Parameter()]
     [ValidateNotNullOrEmpty()]
-    [System.IO.FileInfo]$ConfigFilePath = $env:CITRIX_AUTODEPLOY_CONFIG,
+    [System.IO.FileInfo]$FilePath = $env:CITRIX_AUTODEPLOY_CONFIG,
 
     [Parameter()]
     [ValidateNotNullOrEmpty()]
@@ -15,8 +15,6 @@ param (
     [System.IO.FileInfo]$LogFile = $env:CITRIX_AUTODEPLOY_LOGFILE
 )
 
-Write-VerboseLog -Message "Citrix Autodeploy started via {MyCommand} with parameters: {PSBoundParameters}" -PropertyValues $MyInvocation.MyCommand.Source, ($PSBoundParameters | Out-String)
-
 if (-not $LogLevel) {
     $LogLevel = 'Information'
 }
@@ -25,24 +23,40 @@ Import-Module ${PSScriptRoot}\module\CitrixAutodeploy -Force -ErrorAction Stop -
 
 $Logger = Initialize-CtxAutodeployLogger -LogLevel $LogLevel -LogFile $LogFile
 
+Write-DebugLog -Message "Citrix Autodeploy started via {MyCommand} with parameters: {PSBoundParameters}" -PropertyValues $MyInvocation.MyCommand.Source, ($PSBoundParameters | Out-String)
+
 Initialize-Environment
 
-$Config = Get-CtxAutodeployConfig -FilePath $ConfigFilePath
+$Config = Get-CtxAutodeployConfig -FilePath $FilePath
 
 foreach ($AutodeployMonitor in $Config.AutodeployMonitors.AutodeployMonitor) {
     Write-InfoLog -Message "Starting job: {AutodeployMonitor}" -PropertyValues $AutodeployMonitor
 
-    $AdminAddress  = $AutodeployMonitor.AdminAddress
-    $BrokerCatalog = Get-BrokerCatalog -AdminAddress $AdminAddress -Name $AutodeployMonitor.BrokerCatalog
-    $DesktopGroup  = Get-BrokerDesktopGroup -AdminAddress $AdminAddress -Name $AutodeployMonitor.DesktopGroupName
-    $PreTask       = $AutodeployMonitor.PreTask
-    $PostTask      = $AutodeployMonitor.PostTask
+    $AdminAddress = $AutodeployMonitor.AdminAddress
+    $PreTask      = $AutodeployMonitor.PreTask
+    $PostTask     = $AutodeployMonitor.PostTask
+
+    try {
+        $BrokerCatalog = Get-BrokerCatalog -AdminAddress $AdminAddress -Name $AutodeployMonitor.BrokerCatalog
+    }
+    catch {
+        Write-ErrorLog -Message "Failed to read catalog '{BrokerCatalog}' from delivery controller '{DeliveryController}'" -Exception $_.Exception -ErrorRecord $_ -PropertyValues $AutodeployMonitor.BrokerCatalog, $AutodeployMonitor.AdminAddress
+        continue
+    }
+
+    try {
+        $DesktopGroup = Get-BrokerDesktopGroup -AdminAddress $AdminAddress -Name $AutodeployMonitor.DesktopGroupName
+    }
+    catch [Citrix.Broker.Admin.SDK.SdkOperationException] {
+        Write-ErrorLog -Message "Failed to read catalog '{DesktopGroupName}' from delivery controller '{DeliveryController}'" -Exception $_.Exception -ErrorRecord $_ -PropertyValues $AutodeployMonitor.BrokerCatalog, $AutodeployMonitor.DesktopGroupName, $AutodeployMonitor.AdminAddress
+        continue
+    }
 
     try {
         $UnassignedMachines = Get-BrokerMachine -AdminAddress $AdminAddress -DesktopGroupName $DesktopGroup.Name -IsAssigned $false
     }
     catch {
-        Write-ErrorLog -Message "Failed to get unassigned machines for desktop group '{DesktopGroupName}'" -Exception $_.Exception -ErrorRecord $_ -PropertyValues $AutodeployMonitor.DesktopGroupName
+        Write-ErrorLog -Message "Failed to get unassigned machines for desktop group '{DesktopGroupName}' from delivery controller '{DeliveryController}'" -Exception $_.Exception -ErrorRecord $_ -PropertyValues $AutodeployMonitor.DesktopGroupName, $AutodeployMonitor.AdminAddress
         continue
     }
 
@@ -53,46 +67,74 @@ foreach ($AutodeployMonitor in $Config.AutodeployMonitors.AutodeployMonitor) {
         continue
     }
 
-    Write-VerboseLog -Message "Adding '{MachinesToAdd}' machines to catalog '{BrokerCatalog}'" -PropertyValues $MachinesToAdd, $BrokerCatalog.Name
+    Write-DebugLog -Message "Adding '{MachinesToAdd}' machines to catalog '{BrokerCatalog}'" -PropertyValues $MachinesToAdd, $BrokerCatalog.Name
     while ($MachinesToAdd -gt 0) {
-        try {
-            if ($PreTask) {
-                $PreTaskArgs = @{
-                    'AutodeployMonitor' = $AutodeployMonitor
-                    'DesktopGroup'      = $DesktopGroup
-                    'BrokerCatalog'     = $BrokerCatalog
-                }
-                Write-VerboseLog -Message "Invoking pre-task '{PreTask}'" -PropertyValues $PreTask
+        if ($PreTask) {
+            $PreTaskArgs = @{
+                'AutodeployMonitor' = $AutodeployMonitor
+                'DesktopGroup'      = $DesktopGroup
+                'BrokerCatalog'     = $BrokerCatalog
+            }
+
+            Write-VerboseLog -Message "Invoking pre-task '{PreTask}'" -PropertyValues $PreTask
+
+            try {
                 Invoke-CtxAutodeployTask -FilePath $PreTask -Type Pre -Context "Catalog: $($BrokerCatalog.Name), DesktopGroup: $($DesktopGroup.Name)" -ArgumentList $PreTaskArgs
             }
-
-            $NewVMParams = @{
-                AdminAddress  = $AdminAddress
-                BrokerCatalog = $BrokerCatalog
-                DesktopGroup  = $DesktopGroup
+            catch {
+                continue
             }
-            Write-VerboseLog -Message "Creating new machine for catalog '{BrokerCatalog}'" -PropertyValues $BrokerCatalog.Name
+            finally {
+                $MachinesToAdd--
+            }
+        }
+
+        $NewVMParams = @{
+            AdminAddress  = $AdminAddress
+            BrokerCatalog = $BrokerCatalog
+            DesktopGroup  = $DesktopGroup
+        }
+
+        Write-VerboseLog -Message "Creating new machine for catalog '{BrokerCatalog}'" -PropertyValues $BrokerCatalog.Name
+
+        try {
             $NewBrokerMachine = New-CtxAutodeployVM @NewVMParams
-
-            if ($PostTask) {
-                $PostTaskArgs = @{
-                    'AutodeployMonitor' = $AutodeployMonitor
-                    'DesktopGroup'      = $DesktopGroup
-                    'BrokerCatalog'     = $BrokerCatalog
-                    'NewBrokerMachine'  = $NewBrokerMachine
-                }
-                Write-VerboseLog -Message "Invoking post-task '{PostTask}'" -PropertyValues $PostTask
-                Invoke-CtxAutodeployTask -FilePath $PostTask -Type Post -Context $NewBrokerMachine.MachineName -ArgumentList $PostTaskArgs
-            }
-
-            $MachinesToAdd--
         }
         catch {
             Write-ErrorLog -Message "An error occurred while adding a machine to catalog '{0}'" -Exception $_.Exception -ErrorRecord $_ -PropertyValues $BrokerCatalog.Name
             continue
         }
         finally {
-            $Logger | Close-Logger
+            $MachinesToAdd--
+        }
+
+        if ($PostTask) {
+            $PostTaskArgs = @{
+                'AutodeployMonitor' = $AutodeployMonitor
+                'DesktopGroup'      = $DesktopGroup
+                'BrokerCatalog'     = $BrokerCatalog
+                'NewBrokerMachine'  = $NewBrokerMachine
+            }
+        }
+
+        Write-InfoLog -Message "Invoking post-task '{PostTask}'" -PropertyValues $PostTask
+
+        try {
+            Invoke-CtxAutodeployTask -FilePath $PostTask -Type Post -Context $NewBrokerMachine.MachineName -ArgumentList $PostTaskArgs
+        }
+        catch {
+            continue
+        }
+        finally {
+            $MachinesToAdd--
         }
     }
 }
+
+if ($InternalLogger) {
+    Write-VerboseLog -Message 'Closing internal logger'
+    $InternalLogger | Close-Logger
+}
+
+Write-VerboseLog -Message 'Closing logger'
+$Logger | Close-Logger
