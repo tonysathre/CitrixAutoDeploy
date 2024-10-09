106c813ac1f85ee6abe18162c600c3e2bcfbaa70
tonysathre <tony@ghostbit.org>
2024-10-09 10:06:14 -0500

    fix merge conflict

--- run.ps1
+++ run.ps1
@@ -1,2 +1,19 @@
+#Requires -Modules PoShLog
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [System.IO.FileInfo]$FilePath = $env:CITRIX_AUTODEPLOY_CONFIG,
+
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet('Verbose', 'Debug', 'Information', 'Warning', 'Error', 'Fatal')]
+    [string]$LogLevel = $env:CITRIX_AUTODEPLOY_LOGLEVEL,
+
+    [Parameter()]
+    [ValidateNotNullOrEmpty()]
+    [System.IO.FileInfo]$LogFile = $env:CITRIX_AUTODEPLOY_LOGFILE
+)
+
 $env:CI = $true
 .\citrix_autodeploy.ps1 -ConfigFilePath .\citrix_autodeploy_config.json
