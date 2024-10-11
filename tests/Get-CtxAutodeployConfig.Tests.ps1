84fcab92ffdaf9a089aa303e6df7720c450e6d63
tonysathre <tony@ghostbit.org>
2024-10-11 11:57:19 -0500

    update tests

--- tests/Get-CtxAutodeployConfig.Tests.ps1
+++ tests/Get-CtxAutodeployConfig.Tests.ps1
@@ -1,18 +1,60 @@
 Describe 'Get-CtxAutodeployConfig' {
     BeforeAll {
-        Import-Module ${PSScriptRoot}\..\module\CitrixAutodeploy -Force -ErrorAction Stop -DisableNameChecking -WarningAction SilentlyContinue
+        Import-Module "${PSScriptRoot}\..\module\CitrixAutodeploy" -Force -ErrorAction Stop -DisableNameChecking -WarningAction SilentlyContinue
     }
 
-    It 'Should return configuration object if file exists' {
-        $FilePath = "${PSScriptRoot}/test_config.json"
-        $Config = Get-CtxAutodeployConfig -FilePath $FilePath
-        $Config.AutodeployMonitors.AutodeployMonitor[0].AdminAddress | Should -Be 'test-admin-address'
-
-        #Remove-Item -Path $FilePath
+    Context 'When the configuration file exists' {
+        It 'Should return a configuration object' {
+            $FilePath = "${PSScriptRoot}/test_config.json"
+            $Config = Get-CtxAutodeployConfig -FilePath $FilePath
+            $Config | Should -Not -BeNullOrEmpty
+            $Config.AutodeployMonitors.AutodeployMonitor[0].AdminAddress | Should -Be 'test-admin-address'
+        }
     }
 
-    It 'Should throw error if file does not exist' {
-        $FilePath = "${PSScriptRoot}/non_existent_config.json"
-        { Get-CtxAutodeployConfig -FilePath $FilePath -ErrorAction SilentlyContinue -ErrorVariable Errors } | Should -Throw # suppress error message so it doesn't pollute our terminal
+    Context 'When the configuration file does not exist' {
+        It 'Should throw an error' {
+            $FilePath = "${PSScriptRoot}/non_existent_config.json"
+            { Get-CtxAutodeployConfig -FilePath $FilePath } | Should -Throw
+        }
+    }
+
+    Context 'When the configuration file is invalid JSON' {
+        It 'Should throw an error' {
+            $InvalidJson = @'
+{
+    "AutodeployMonitors": {
+        "AutodeployMonitor": [
+            {
+                "AdminAddress": "test-admin-address",
+                "BrokerCatalog": "test-broker-catalog",
+                "DesktopGroupName": "test-desktop-group-name",
+        }
+    }
+}
+'@ | Set-Content -Path "${PSScriptRoot}/invalid_config.json"
+            $FilePath = "${PSScriptRoot}/invalid_config.json"
+            { Get-CtxAutodeployConfig -FilePath $FilePath } | Should -Throw
+        }
+
+        AfterAll {
+            Remove-Item "${PSScriptRoot}/invalid_config.json"
+        }
+    }
+
+    Context 'When no FilePath is provided and environment variable is set' {
+        It 'Should use the environment variable for the file path' {
+            $env:CITRIX_AUTODEPLOY_CONFIG = "${PSScriptRoot}/test_config.json"
+            $Config = Get-CtxAutodeployConfig
+            $Config | Should -Not -BeNullOrEmpty
+            $Config.AutodeployMonitors.AutodeployMonitor[0].AdminAddress | Should -Be 'test-admin-address'
+        }
+    }
+
+    Context 'When no FilePath is provided and environment variable is not set' {
+        It 'Should throw an error' {
+            $env:CITRIX_AUTODEPLOY_CONFIG = $null
+            { Get-CtxAutodeployConfig } | Should -Throw
+        }
     }
 }
