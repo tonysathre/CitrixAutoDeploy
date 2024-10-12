f1adbcf6c67504cf6ad083df291f7004c1a11e85
tonysathre <tony@ghostbit.org>
2024-10-12 14:43:48 -0500

    rename file: Test-MaxMachineCount.ps1 -> Test-MachineCountLimit.ps1

--- module/CitrixAutodeploy/functions/public/Test-MachineCountLimit.ps1
+++ module/CitrixAutodeploy/functions/public/Test-MachineCountLimit.ps1
@@ -1,4 +1,4 @@
-function Test-MaxMachineCount {
+function Test-MachineCountLimit {
     [CmdletBinding()]
     [OutputType([bool])]
     param (
@@ -24,8 +24,11 @@ function Test-MaxMachineCount {
 
     if ($InputObject -is [Citrix.Broker.Admin.SDK.Catalog]) {
         $Params.Add('CatalogName', $InputObject.Name)
-    } else {
+    } elseif ($InputObject -is [Citrix.Broker.Admin.SDK.DesktopGroup]) {
         $Params.Add('DesktopGroupName', $InputObject.Name)
+    } else {
+        Write-ErrorLog -Message "Input object is not a valid type. Expected InputObject to be of type Citrix.Broker.Admin.SDK.Catalog or Citrix.Broker.Admin.SDK.DesktopGroup. Got {InputObject}" -PropertyValues $InputObject.GetType().FullName
+        throw
     }
 
     try {
